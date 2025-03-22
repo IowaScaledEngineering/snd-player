c19c97df749776662cf35d63457c0c1a4a1d9c2a
Michael Petersen <railfan@drgw.net>
2025-03-21 21:48:15 -0600

    Fixed errors due to updated API in IDF 5.3.2. Updated to new I2S driver.

--- src/snd-player/snd-player.ino
+++ src/snd-player/snd-player.ino
@@ -21,14 +21,12 @@ LICENSE:
 
 #include <SPI.h>
 #include <SD.h>
-#include "driver/i2s.h"
 #include <Preferences.h>
 #include <vector>
 #include <strings.h>
+#include "driver/i2s_std.h"
+#include "driver/gpio.h"
 #include "esp_task_wdt.h"
-
-// 10 sec WDT
-#define WDT_TIMEOUT 10
 
 #include "sound.h"
 
@@ -36,9 +34,14 @@ LICENSE:
 #include "squeal/flangeClip02.h"
 #include "squeal/flangeClip03.h"
 
+// 3 sec watchdog 
+#define TWDT_TIMEOUT_MS    3000
+
 // Samples for each buffer
 #define AUDIO_BUFFER_SIZE   512
 #define AUDIO_BUFFER_NUM      4
+
+i2s_chan_handle_t i2s_tx_handle;
 
 // Pins
 #define EN1         9
@@ -55,9 +58,9 @@ LICENSE:
 #define AUX4       18
 #define AUX5        8
 #define I2S_SD      4
-#define I2S_DATA    5
-#define I2S_BCLK    6
-#define I2S_LRCLK   7
+#define I2S_DOUT    GPIO_NUM_5
+#define I2S_BCLK    GPIO_NUM_6
+#define I2S_LRCLK   GPIO_NUM_7
 #define SDCLK      36
 #define SDMOSI     35
 #define SDMISO     37
@@ -241,13 +244,17 @@ void setup()
 	pinMode(AUX4, OUTPUT);
 	pinMode(AUX5, OUTPUT);
 
-	esp_task_wdt_init(WDT_TIMEOUT, true);  // 1s timeout
-	esp_task_wdt_add(NULL); //add current thread to WDT watch
+	esp_task_wdt_config_t twdt_config = {
+		.timeout_ms = TWDT_TIMEOUT_MS,
+		.idle_core_mask = (1 << CONFIG_FREERTOS_NUMBER_OF_CORES) - 1,    // Bitmask of all cores
+		.trigger_panic = false,
+	};
+	esp_task_wdt_init(&twdt_config);
+    	esp_task_wdt_add(NULL); //add current thread to WDT watch
 	esp_task_wdt_reset();
 
-	timer = timerBegin(0, 80, true);	// Timer 0, 80x prescaler = 1us
-	timerAttachInterrupt(timer, &processVolume, false);  // level triggered
-	timerAlarmWrite(timer, 10000, true);	// 80MHz / 80 / 10000 = 10ms, autoreload
+	timer = timerBegin(1000000);                  // 1MHz = 1us
+	timerAttachInterrupt(timer, &processVolume);
 }
 
 typedef enum
@@ -265,7 +272,7 @@ void playerInit(void)
 	playerState = PLAYER_IDLE;
 }
 
-void play(i2s_port_t i2s_num)
+void play(i2s_chan_handle_t i2s_handle)
 {
 	size_t i;
 	int16_t sampleValue;
@@ -302,7 +309,7 @@ digitalWrite(AUX2, 0);
 				// Combine into 32 bit word (left & right)
 				outputValue = (sampleValue<<16) | (sampleValue & 0xffff);
 digitalWrite(AUX3, 1);
-				i2s_write(i2s_num, &outputValue, 4, &bytesWritten, 1);
+				i2s_channel_write(i2s_handle, &outputValue, 4, &bytesWritten, 1);
 				if(0 == bytesWritten)
 					playerState = PLAYER_RETRY;
 digitalWrite(AUX3, 0);
@@ -324,7 +331,7 @@ digitalWrite(AUX3, 0);
 			break;
 		case PLAYER_RETRY:
 digitalWrite(AUX4, 1);
-			i2s_write(i2s_num, &outputValue, 4, &bytesWritten, 1);
+			i2s_channel_write(i2s_handle, &outputValue, 4, &bytesWritten, 1);
 			if(0 == bytesWritten)
 				playerState = PLAYER_RETRY;
 			else
@@ -336,7 +343,7 @@ digitalWrite(AUX4, 0);
 			outputValue = 0;
 			for(i=0; i<AUDIO_BUFFER_SIZE; i++)
 			{
-				i2s_write(i2s_num, &outputValue, 4, &bytesWritten, 1);
+				i2s_channel_write(i2s_handle, &outputValue, 4, &bytesWritten, 1);
 				// If it times out, then the buffer is full
 			}
 			break;
@@ -366,7 +373,6 @@ void loop()
 	esp_task_wdt_reset();
 	
 	playerInit();
-	timerAlarmDisable(timer);
 
 	Serial.println("ISE Sound Player");
 
@@ -580,36 +586,32 @@ void loop()
 		esp_task_wdt_reset();
 	}
 
-	timerAlarmEnable(timer);
+	timerAlarm(timer, 10000, true, 0);            // 1us * 10000 = 10ms, autoreload, unlimited reloads
 
-	i2s_port_t i2s_num = I2S_NUM_0;
-	i2s_config_t i2s_config = {
-			.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-			.sample_rate = 44100,
-//			.sample_rate = 8000,
-			.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-			.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-			.communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_STAND_I2S,
-			.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-			.dma_buf_count = AUDIO_BUFFER_NUM,
-			.dma_buf_len = AUDIO_BUFFER_SIZE,
-			.use_apll = 0,
-			.tx_desc_auto_clear = true,
-			.fixed_mclk = -1,
-			.mclk_multiple = I2S_MCLK_MULTIPLE_DEFAULT,
-			.bits_per_chan = I2S_BITS_PER_CHAN_DEFAULT,
+	// Default dma_frame_num = 240 (i2s_common.h)
+	//    Total DMA size = 240 * 2 * 16 / 8 = 960 bytes
+	i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+	i2s_new_channel(&chan_cfg, &i2s_tx_handle, NULL);
+
+	i2s_std_config_t std_cfg = {
+		.clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(44100),
+		.slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
+		.gpio_cfg = {
+			.mclk = I2S_GPIO_UNUSED,
+			.bclk = I2S_BCLK,
+			.ws = I2S_LRCLK,
+			.dout = I2S_DOUT,
+			.din = I2S_GPIO_UNUSED,
+			.invert_flags = {
+				.mclk_inv = false,
+				.bclk_inv = false,
+				.ws_inv = false,
+			},
+		},
 	};
 
-	i2s_pin_config_t pin_config = {
-			.mck_io_num = I2S_PIN_NO_CHANGE,
-			.bck_io_num = I2S_BCLK,
-			.ws_io_num = I2S_LRCLK,
-			.data_out_num = I2S_DATA,
-			.data_in_num = I2S_PIN_NO_CHANGE,
-	};
-
-	i2s_driver_install(i2s_num, &i2s_config, 0, NULL);
-	i2s_set_pin(i2s_num, &pin_config);
+	i2s_channel_init_std_mode(i2s_tx_handle, &std_cfg);
+	i2s_channel_enable(i2s_tx_handle);
 
 	digitalWrite(I2S_SD, 1);	// Enable amplifier
 
@@ -857,7 +859,7 @@ void loop()
 
 digitalWrite(AUX1, 1);
 		// Pump the sound player
-		play(i2s_num);
+		play(i2s_tx_handle);
 digitalWrite(AUX1, 0);
 
 
@@ -873,5 +875,6 @@ digitalWrite(AUX1, 0);
 
 	// Never get here but this is what we would do to clean up
 	digitalWrite(I2S_SD, 0);	// Disable amplifier
-	i2s_driver_uninstall(i2s_num);
+	i2s_channel_disable(i2s_tx_handle);
+	i2s_del_channel(i2s_tx_handle);
 }
